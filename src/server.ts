d3b28e9ec4ba803309332dbaac4cd39ac2bca6ef
Lucas EStevam <dev.lucasestevam@gmail.com>
2022-05-10 21:04:08 -0300

    :art: change routes structures

--- src/server.ts
+++ src/server.ts
@@ -15,8 +15,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1/webhook", webhookRouter);
-app.use("/api/v1/user", userRouter);
+app.use("/webhook", webhookRouter);
+app.use("/user", userRouter);
 app.use(errorHandler);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
